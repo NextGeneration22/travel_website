e8a80d7399e48069f82917b936a38d85e5d5bd96
NextGeneration22 <ayoubejraidi@gmail.com>
2024-12-21 05:33:12 -0500

    add code to fetch data from .json

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -4,6 +4,41 @@ let dataArray= [];
 function findDestination(){
     const searchinput = document.getElementById("searchbar").value;
     fetch('travel_recommendation_api.json')
-      .the(response => response.json)
-      .then(data=>{})
+      .then(response => response.json())
+      .then(data=>{
+        searchResult.innerHTML = '';
+        switch(searchinput){
+            case 'countries':
+                data.countries.forEach(element => {
+                    searchResult.innerHTML += `<h2>${element.name}</h2>`;
+                    element.cities.forEach(item=>{    
+                        searchResult.innerHTML += `<h3>${item.name}</h3>`
+                        searchResult.innerHTML += `<img src="${item.imageUrl}" id='city'/>`
+                        searchResult.innerHTML += `<pre>${item.description}</pre>`
+                    })
+                });
+                break;
+            case 'temples':
+                data.temples.forEach(element=>{
+                    searchResult.innerHTML += `<img src="${element.imageUrl}"/>`;
+                    searchResult.innerHTML += `<h2>${element.name}</h2>`;
+                    searchResult.innerHtml += `<pre>${element.description}</pre>`;
+                });
+                break;
+            case 'beaches':
+                data.beaches.forEach(element=>{
+                    searchResult.innerHTML += `<img src="${element.imageUrl}/>`;
+                    searchResult.innerHTML += `<h2>${element.name}</h2>`
+                    searchResult.innerHTML += `<pre>${element.description}</pre>`
+                });
+                break;
+            default:
+                alert('sorry we don\'t offer travel to this destination yet!')
+            }
+      }).catch(error => {
+         console.error('Error fetching data:', error); 
+         alert('Failed to fetch data. Please try again later.'); 
+    });
 }
+
+document.getElementById('search').addEventListener('click', findDestination);
